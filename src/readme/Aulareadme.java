500653f17045930b2bcf3a333990abb00ef2a868
thiagomunizz <thiagomuniz88@hotmail.com>
2018-03-01 20:54:13 -0300

    exercicios resolvidos

---
+++ src/readme/Aulareadme.java
@@ -0,0 +1,14 @@
+package readme;
+
+public class Aulareadme {
+	public static void main ( String[] args){
+	                                              
+	int resto
+	int resto = 99 % 4;
+	
+	System.out.println(\n"
+	
+	}
+	
+
+}
